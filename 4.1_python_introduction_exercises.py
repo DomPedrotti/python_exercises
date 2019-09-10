957a8773461cb3bea21c454a3836faf7e33b9431
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-09-10 11:45:48 -0500

    intro exercise first commit

---
+++ 4.1_python_introduction_exercises.py
@@ -0,0 +1,8 @@
+# Create a hello world program. Create a text file named 4.1_python_introduction_exercises.py and write a program that prints "Hello, World!" to the console. Run this program from the command line.
+# Inside of your hello world program, create a variable named greeting that contains the message that you will print to the console. Run your script interactively and view the contents of the greeting variable.
+
+print('hello world')
+
+greeting = 'hello world, and people, and plants, and everything!'
+
+print(greeting)
